91ab7830865e8d561192a28b9a3d4f24f37931a9
naveenVemuri <nvemuri@neptunetg.com>
2022-10-26 19:16:46 +0530

    firstcommit

---
+++ CICD_Testing/UnitTest1.cs
@@ -0,0 +1,61 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CICD_Testing
+{
+    public class Tests
+    {
+        private static IWebDriver uniqueInstanceWebDriver = null;
+        [SetUp]
+        public void Setup()
+        {
+            string appdatapath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appdatapath = appdatapath.Replace("Roaming", "");
+            ChromeOptions options = new ChromeOptions();
+            options.AcceptInsecureCertificates = true;
+            options.AddArgument("--ignore-ssl-errors=yes");
+            options.AddArgument("--ignore-certificate-errors");
+            options.AddArgument(string.Format("user-data-dir={0}", appdatapath + "Google\\Chrome\\User Data"));
+            options.AddUserProfilePreference("download.prompt_for_download", true);
+            options.AddArguments("--disable-extensions");
+            uniqueInstanceWebDriver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            uniqueInstanceWebDriver.Url = "https://youtube.com/";
+        }
+
+        [Test]
+        public void Test2()
+        {
+            uniqueInstanceWebDriver.Url = "https://youtube.com/";
+        }
+
+        [Test]
+        public void Test3()
+        {
+            uniqueInstanceWebDriver.Url = "https://youtube.com/";
+        }
+
+        [Test]
+        public void Test4()
+        {
+            uniqueInstanceWebDriver.Url = "https://youtube.com/";
+        }
+
+        [Test]
+        public void Test5()
+        {
+            uniqueInstanceWebDriver.Url = "https://youtube.com/";
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            uniqueInstanceWebDriver.Quit();
+        }
+    }
+}
